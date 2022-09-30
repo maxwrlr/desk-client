455f7f75324444bf5796e4164ebb8c30b3e9a738
maxwrlr <maxwrlr@github.com>
2022-09-30 09:59:59 +0200

    init macOS app

---
+++ macOS/Schreibtisch/SchreibtischApp.swift
@@ -0,0 +1,15 @@
+//
+//  SchreibtischApp.swift
+//  Schreibtisch
+//
+
+import SwiftUI
+
+@main
+struct SchreibtischApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
